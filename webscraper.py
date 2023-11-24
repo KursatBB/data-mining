9122ff95f921106205772fbd8ebcc689b4cca30c
Kürşat <kursat.babur3419@gop.edu.tr>
2023-11-24 20:32:16 +0300

    Verileri Çekerken Başlık ile Çekmesi Eklendi
    + Veriler önceden düz bir şekilde sadece ürün ve fiyatı ile çekiliyordu. Artık ana kategori altındaki yeni gelenler, çok satanlar gibi alt başlıklar ile birlikte çekiliyorlar.
    
    + Ürün listesini yazdırdığı dosyayı üst dizine değil artık bulunduğu dizine yazdırıyor.

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,6 @@
 import requests
+import os
 from bs4 import BeautifulSoup
-
 def get_product_info(url, output_file):
     # Web sitesinden sayfayı indir
     response = requests.get(url)
@@ -10,26 +10,45 @@ def get_product_info(url, output_file):
         # HTML içeriğini BeautifulSoup ile işle
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Ürünlerin bulunduğu etiketleri belirle
-        product_tags = soup.find_all('a', class_='product-box')
+        # Başlıkları ve ilgili ürünleri bul
+        categories = soup.find_all('div', class_='headline')
 
-        # Ürün bilgilerini dosyaya yaz
+        # Dosyayı yazma modunda aç
         with open(output_file, 'w', encoding='utf-8') as file:
-            for product_tag in product_tags:
-                # Ürün Adı
-                product_name_tag = product_tag.find('div', class_='product-box-container-name')
-                product_name = product_name_tag.text.strip() if product_name_tag else None
 
-                # Fiyat
-                price_tag = product_tag.find('div', class_='price-box-price')
-                price = price_tag.text.strip() if price_tag else None
+            # Her bir başlık için ürünleri çek
+            for category in categories:
+                # Başlık adını çek
+                category_name = category.find('h2').text.strip()
 
-                # Ürün bilgilerini dosyaya yaz
-                file.write(f"Ürün Adı: {product_name}\n")
-                file.write(f"Fiyat: {price}\n")
-                file.write("-" * 30 + "\n")
+                        # Dosya yolunu oluştur
+                script_directory = os.path.dirname(os.path.abspath(__file__))
+                output_file_path = os.path.join(script_directory, output_file)
 
-        print(f"Veriler '{output_file}' dosyasına yazıldı.")
+
+                # Başlığı dosyaya yaz
+                file.write(f"\n{category_name}\n{'=' * len(category_name)}\n")
+
+                # İlgili ürünleri seç
+                product_container = category.find_next('div', class_='slider-product')
+                product_tags = product_container.find_all('a', class_='product-box')
+
+                # Her bir ürün için bilgileri dosyaya yaz
+                for product_tag in product_tags:
+                    # Ürün Adı
+                    product_name_tag = product_tag.find('div', class_='product-box-container-name')
+                    product_name = product_name_tag.text.strip() if product_name_tag else None
+
+                    # Fiyat
+                    price_tag = product_tag.find('div', class_='price-box-price')
+                    price = price_tag.text.strip() if price_tag else None
+
+                    # Ürün bilgilerini dosyaya yaz
+                    file.write(f"Ürün Adı: {product_name}\n")
+                    file.write(f"Fiyat: {price}\n")
+                    file.write("-" * 30 + "\n")
+
+        print(f"Veriler '{output_file_path}' dosyasına yazıldı.")
 
     else:
         print(f"Error: {response.status_code}")
@@ -42,90 +61,3 @@ output_file = "product_info.txt"
 
 # Fonksiyonu çağır
 get_product_info(category_url, output_file)
-
-
-
-""" import requests
-from bs4 import BeautifulSoup
-
-def get_product_info(url):
-    # Web sitesinden sayfayı indir
-    response = requests.get(url)
-
-    # HTTP yanıt kodunu kontrol et
-    if response.status_code == 200:
-        # HTML içeriğini BeautifulSoup ile işle
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Ürünlerin bulunduğu HTML etiketlerini belirle
-        product_tags = soup.find_all('div', class_='product')
-
-        # Her bir ürün için bilgileri çek
-        for product_tag in product_tags:
-            # Ürün Adı
-            product_name_tag = product_tag.find('div', class_='product-box-container-name')
-            product_name = product_name_tag.text.strip() if product_name_tag else None
-
-            # Fiyat
-            price_tag = product_tag.find('div', class_='price-box-price')
-            price = price_tag.text.strip() if price_tag else None
-
-            # Ürün bilgilerini yazdır veya başka bir işlem yap
-            print(f"Ürün Adı: {product_name}")
-            print(f"Fiyat: {price}")
-            print("-" * 30)
-
-    else:
-        print(f"Error: {response.status_code}")
-
-# Kategori sayfasının URL'si
-category_url = "https://www.pttavm.com/elektronik"
-
-# Fonksiyonu çağır
-get_product_info(category_url) """
-
-
-
-
-
-
-
-
-
-
-""" import requests
-from bs4 import BeautifulSoup
-
-def get_product_info(url):
-    # Web sitesinden sayfayı indir
-    response = requests.get(url)
-
-    # HTTP yanıt kodunu kontrol et
-    if response.status_code == 200:
-        # HTML içeriğini BeautifulSoup ile işle
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Ürünlerin bulunduğu HTML etiketlerini belirle
-        product_tags = soup.find_all('div', class_='product-box p-1')
-
-        # Her bir ürün için bilgileri çek
-        for product_tag in product_tags:
-            product_name_tag = soup.find('div', class_='product-box-container-name')
-            product_name = product_name_tag.text.strip() if product_name_tag else None
-            price_tag = soup.find('div', class_='price-box-price')
-            price = price_tag.text.strip() if price_tag else None
-
-            # Ürün bilgilerini yazdır veya başka bir işlem yap
-            print(f"Ürün Adı: {product_name}")
-            print(f"Fiyat: {price}")
-
-            print("-" * 30)
-
-    else:
-        print(f"Error: {response.status_code}")
-
-# Kategori sayfasının URL'si
-category_url = "https://www.pttavm.com/elektronik"
-
-# Fonksiyonu çağır
-get_product_info(category_url) """
